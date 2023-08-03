9cce7e214f3fe825ccae1d7ce6d1fdcf2a576420
Brian <britabyzk29@gmail.com>
2023-08-03 18:54:36 +0300

    create a middleware and my info route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 import axios from "axios";
 import Link from "next/link";
+import React from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 export default function ProfilePage() {
     const router = useRouter();
+    const [data, setData] = React.useState("nothing")
     const logout = () => {
         try {
             axios.get("/api/users/logout")
@@ -20,15 +22,23 @@ export default function ProfilePage() {
         }
 
     }
+    const getUserDetails = async () => {
+        const res = await axios.get("/api/users/myinfo")
+        console.log(res.data.data._id)
+        setData(res.data.data._id)
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2 className="text-sky-400 under">{data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
             <hr />
 
             <button type="button" className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 onClick={logout}>Logout</button>
+            <button type="button" className="bg-orange-500 mt-4 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+                onClick={getUserDetails}>Get user details</button>
         </div>
     )
 }
